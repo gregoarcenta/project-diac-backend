3623e868e1fc684aefef823e180955039bf3836e
gregory arcentales <gregoarcenta@gmail.com>
2021-12-07 12:53:53 -0500

    Se agregan mas columnas al modelo del documento curricular

--- models/Curricular.js
+++ models/Curricular.js
@@ -13,6 +13,66 @@ Curricular.init({
     duration: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    infoPedagogica: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    developData: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    familyBack: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    historySchool: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    styleLearning: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    typeIntelligence: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dataContextEducation: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    dataContextFamily: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    dataContextSocial: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    identificationEducationalNeed: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    adaptationAccessCurriculum: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    specializedIntExt: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    methodology: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    resourse: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    resultFinal: {
+        type: DataTypes.TEXT,
+        allowNull: false
     }
 }, {
     sequelize: db,
