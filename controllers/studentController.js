7d80a0cabb6169869368fa4a12a79e8ed7a0d0fc
gregory arcentales <gregoarcenta@gmail.com>
2022-01-11 15:02:24 -0500

    Se actualiza contrladores para middleware next de error

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,47 +1,46 @@
 const Student = require('../models/Student')
 
-const index = async (req, res) => {
+const index = async (req, res, next) => {
     try {
         const students = await Student.findAll()
         res.json({
             students
         })
     } catch (error) {
-        throw new Error(error)
+        next(error)
     }
 
 }
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
     try {
         const student = await Student.create({ ...req.body })
         res.json(student)
     } catch (error) {
-        res.json(error)
-        throw new Error(error)
+        next(error)
     }
 }
 
-const update = async (req, res) => {
+const update = async (req, res, next) => {
     const id = req.params.id
     try {
         const student = await Student.findOne({ where: { id } })
         const newStudent = await student.update({ ...req.body })
         res.json(newStudent)
     } catch (error) {
-        throw new Error(error)
+        next()
     }
 
 }
 
-const destroy = async (req, res) => {
+const destroy = async (req, res, next) => {
     const id = req.params.id
     try {
         const student = await Student.findOne({ where: { id } })
         const studentDelete = await student.destroy()
         res.json(studentDelete)
     } catch (error) {
-        throw new Error(error)
+        next(error)
     }
 
 }
--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,49 +1,48 @@
 const Course = require('../models/Course')
 const Teacher = require('../models/Teacher')
 
-const index = async (req, res) => {
+const index = async (req, res, next) => {
     try {
         const teachers = await Teacher.findAll({ include: Course })
         res.json({
             teachers
         })
     } catch (error) {
-        throw new Error(error)
+        next(error)
     }
 
 }
 
-const create = async (req, res) => {
+const create = async (req, res, error) => {
     const CourseId = req.body.courseId
     try {
         const teacher = await Teacher.create({ ...req.body, CourseId })
         res.json(teacher)
     } catch (error) {
-        res.json(error)
-        throw new Error(error)
+        next(error)
     }
 }
 
-const update = async (req, res) => {
+const update = async (req, res, next) => {
     const id = req.params.id
     try {
         const teacher = await Teacher.findOne({ where: { id } })
         const newTeacher = await teacher.update({ ...req.body })
         res.json(newTeacher)
     } catch (error) {
-        throw new Error(error)
+        next(error)
     }
 
 }
 
-const destroy = async (req, res) => {
+const destroy = async (req, res, next) => {
     const id = req.params.id
     try {
         const teacher = await Teacher.findOne({ where: { id } })
         const teacherDelete = await teacher.destroy()
         res.json(teacherDelete)
     } catch (error) {
-        throw new Error(error)
+        next(error)
     }
 
 }
